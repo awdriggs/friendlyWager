976a6df2a82629092b15c0797e74768a2a18c787
adam driggers <awdriggs@gmail.com>
2015-07-18 11:16:15 -0400

    setup node server, layout.hanldebars and home handlebars

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # friendlyWager
 Forum with ability to set wagers on when events will happen
 
+## Resources
 [Simple Prototype](https://popapp.in/w/projects/55a944063df1d6955c069f6b/mockups)
 
 [Trello Workflow](https://trello.com/b/beBxU5mk/friendly-wager) 
+
+![wireframe](https://github.com/awdriggs/friendlyWager/blob/master/resources/wireframe001.jpg "Wireframe")
 
---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var methodOverride = require('method-override');
+var logger = require('morgan');
+var path = require('path');
+
+var fs = require('fs');
+
+app.listen(3000);
+
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main',
+	extname: 'handlebars'
+}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded());
+app.use(express.static('public'));
+app.use(logger('dev'));
+
+app.use(methodOverride(function(req, res){
+	if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+		var method = req.body._method;
+		delete req.body._method;
+		return method;
+	}
+}));
+
+fs.readdirSync('./controllers').forEach(function (file) {
+	if(file.substr(-3) == '.js') {
+		route = require('./controllers/' + file);
+		route.controller(app);
+	}
+});
+
+// HOME route
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
